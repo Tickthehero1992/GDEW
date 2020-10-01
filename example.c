bfb729516f8b3f939a1eec8886d79fcfee0b4cbd
Tickthehero1992 <pereirasanchez@mail.ru>
2020-10-01 16:24:16 +0300

    delete directories

--- example.c
+++ example.c
@@ -1,10 +1,7 @@
 #include "main.h"
 #include "GDEWDotDisp.h"
-/***** for test u shotld configure your ports in Display_EPD_W21.h*************/
-/******U should remake definitions for work with ports*************************/
-/******In Display_EPD_W21.c u need to edit Port_Configs()**********************
-*******function to configue input/output ports*********************************
-******************************************************************************/
+
+/******************STM32 example*******/
 int main()
 {
 /*************************Your configs******************************************************/
@@ -13,20 +10,32 @@ Port_Configs();
 /*******************Now default parameters for GDEW Disp***************************************/
 /*******************This parameters was tested in stm32f0Discovery with display GDEW0213T5*/
 
+    GDESPI SPI = { GPIO_PIN_8, GPIOA, //MISO
+		  GPIO_PIN_5,GPIOA, //MOSI
+		  GPIO_PIN_7, GPIOA, //SCK
+		  GPIO_PIN_4,GPIOA,  //CSS
+		  GPIO_PIN_2,GPIOA,  //DC
+		  GPIO_PIN_1, GPIOA, //RST
+		  GPIO_PIN_3, GPIOA, //BS
+		  0X01
+  };
+
+  
   GDEW_POWER GDEW_POW=GDEW_POWER_DEFAULT;
   GDEW_PSR PSR=GDEW_PSR_DEFAULT;
   GDEW_BST_STR BST=GDEW_BST_DEFAULT;
   GDEW_PLL PLL=GDEW_PLL_DEFAULT;
   GDEW_VCOM_DATA VCOM =GDEW_VCOM_DATA_DEFAULT;
-  GDEW GDE={0x03, 104,212,PSR,GDEW_POW,BST,PLL,VCOM};
-  
+  GDEW GDE={0x03, 104,212,PSR,GDEW_POW,BST,PLL,VCOM, SPI};
+  GDESPI_INIT(GDE.SPI);
+
   while(1)
   {
-	GDEW_INIT(GDE);
-	GDEW_PICTURE_WRITE(gImage,gImage,GDE.height, GDE.width);
-        GDEW_DEEP_SLEEP;
-	driver_delay_xms(15000);
-	GDEW_DISPLAY_CLEAR(GDE);
+  	  GDEW_INIT(GDE);
+	  GDEW_PICTURE_WRITE(GDE,gImage,gImage,GDE.height, GDE.width);
+	  GDEW_DEEP_SLEEP(GDE);
+	  HAL_Delay(10000);
+	  GDEW_DISPLAY_CLEAR(GDE);
 
   }
 return 0;
